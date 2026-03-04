977e93c324bc0eca4a91158c19fef4a7ce9de0be
Gollyx Orchestrator <orchestrator@golly.life>
2026-03-04 00:35:02 +0000

    Add Route 53 record for Star VII Cup (cloud.star.vii.golly456.life)

--- terraform/golly456_life.tf
+++ terraform/golly456_life.tf
@@ -826,3 +826,15 @@ resource "aws_api_gateway_base_path_mapping" "golly456_life_apigw_cloud_star_vii
   stage_name  = "integration"
   domain_name = aws_api_gateway_domain_name.golly456_life_apigw_cloud_star_vii.domain_name
 }
+
+resource "aws_route53_record" "golly456_life_cloud_star_vii" {
+  zone_id         = local.golly456_life_zone_id
+  name            = "cloud.star.vii.golly456.life"
+  type            = "A"
+  allow_overwrite = true
+  alias {
+    name                   = "dhuidg3ekucji.cloudfront.net"
+    zone_id                = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
