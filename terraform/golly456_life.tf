201e2c2d2ff2a41070dbe41fb2ff0f5293191e88
Gollyx Orchestrator <orchestrator@golly.life>
2026-08-10 03:07:18 +0000

    Add Route 53 record for Hellmouth VIII Cup (cloud.viii.golly456.life)

--- terraform/golly456_life.tf
+++ terraform/golly456_life.tf
@@ -910,3 +910,15 @@ resource "aws_api_gateway_base_path_mapping" "golly456_life_apigw_cloud_viii" {
   stage_name  = "integration"
   domain_name = aws_api_gateway_domain_name.golly456_life_apigw_cloud_viii.domain_name
 }
+
+resource "aws_route53_record" "golly456_life_cloud_viii" {
+  zone_id         = local.golly456_life_zone_id
+  name            = "cloud.viii.golly456.life"
+  type            = "A"
+  allow_overwrite = true
+  alias {
+    name                   = "d2xhza7lsfwxuy.cloudfront.net"
+    zone_id                = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
